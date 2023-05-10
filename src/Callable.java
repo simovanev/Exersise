1978c5e3e1d4b27ef899b7fc313a12fd3166fdd1
simovanev <simovanev@abv.bg>
2023-05-10 09:01:39 +0300

    Initial commit

---
+++ src/Callable.java
@@ -0,0 +1,5 @@
+public interface Callable {
+    void call(String number);
+
+
+}
---
+++ src/Smartphone.java
@@ -0,0 +1,15 @@
+public class Smartphone implements Callable,Browsable{
+
+
+
+    
+    @Override
+    public void browse(String url) {
+
+    }
+
+    @Override
+    public void call(String number) {
+
+    }
+}
